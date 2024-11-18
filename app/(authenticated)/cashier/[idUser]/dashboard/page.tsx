54c88461bf224a2eeb39acf2a7391a29810918c2
samuelsilaenok <148195581+samuelsilaenok@users.noreply.github.com>
2024-11-18 12:30:38 +0700

    resolve

--- app/(authenticated)/cashier/[idUser]/dashboard/page.tsx
+++ app/(authenticated)/cashier/[idUser]/dashboard/page.tsx
@@ -1,156 +1,63 @@
+// components/Dashboard.tsx
 "use client";
-import { Row, Col, Card, Statistic } from 'antd';
-import { DollarOutlined, ShoppingCartOutlined, UserOutlined, FileTextOutlined, BarChartOutlined } from '@ant-design/icons';
+import React from "react";
+import { ClockCircleOutlined } from "@ant-design/icons";
 
-export default function DashboardPage() {
-  return (
-
-    <div style={{ padding: '16px' }}>
-      {/* Bagian atas: 3 kotak */}
-      <Row gutter={[16, 16]}>
-        <Col span={8}>
-          <Card
-            style={{ 
-              backgroundColor: '#FFFFFF', 
-              borderRadius: '8px', 
-              padding: '8px',
-              border: '3px solid #E2E8F0',
-              minHeight: '130px'
-            }}
-          >
-            <div style={{ textAlign: 'left' }}>
-              <div style={{ backgroundColor: '#543310', borderRadius: '8px', padding: '8px', marginBottom: '8px', display: 'inline-block' }}>
-                <ShoppingCartOutlined style={{ color: '#FFFFFF', fontSize: '38px' }} />
-              </div>
-              <Statistic
-                title={<h4 style={{ fontSize: '16px', color: '#543310' }}>Total Category</h4>}
-                value={10} // Mengisi nilai dari data API
-                valueStyle={{ color: '#64748B', fontSize: '18px', fontWeight: 'bold' }}
-              />
-            </div>
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            style={{ 
-              backgroundColor: '#FFFFFF', 
-              borderRadius: '8px', 
-              padding: '8px',
-              border: '3px solid #E2E8F0',
-              minHeight: '130px'
-            }}
-          >
-            <div style={{ textAlign: 'left' }}>
-              <div style={{ backgroundColor: '#543310', borderRadius: '8px', padding: '8px', marginBottom: '8px', display: 'inline-block' }}>
-                <ShoppingCartOutlined style={{ color: '#FFFFFF', fontSize: '38px' }} />
-              </div>
-              <Statistic
-                title={<h4 style={{ fontSize: '16px', color: '#543310' }}>Total Product</h4>}
-                value={10} // Mengisi nilai dari data API
-                valueStyle={{ color: '#64748B', fontSize: '18px', fontWeight: 'bold' }}
-              />
-            </div>
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            style={{ 
-              backgroundColor: '#FFFFFF', 
-              borderRadius: '8px', 
-              padding: '8px',
-              border: '3px solid #E2E8F0',
-              minHeight: '130px'
-            }}
-          >
-            <div style={{ textAlign: 'left' }}>
-              <div style={{ backgroundColor: '#543310', borderRadius: '8px', padding: '8px', marginBottom: '8px', display: 'inline-block' }}>
-                <UserOutlined style={{ color: '#FFFFFF', fontSize: '38px' }} />
-              </div>
-              <Statistic
-                title={<h4 style={{ fontSize: '16px', color: '#543310' }}>Total Cashier</h4>}
-                value={10} // Mengisi nilai dari data API
-                valueStyle={{ color: '#64748B', fontSize: '18px', fontWeight: 'bold' }}
-              />
-            </div>
-          </Card>
-        </Col>
-      </Row>
-
-      {/* Bagian tengah: 2 kotak */}
-      <Row gutter={[16, 16]} style={{ marginTop: '16px' }}>
-        <Col span={8}>
-          <Card
-            style={{ 
-              backgroundColor: '#FFFFFF', 
-              borderRadius: '8px', 
-              padding: '8px', 
-              minHeight: '130px',
-              border: '3px solid #E2E8F0'
-            }}
-          >
-            <div style={{ textAlign: 'left' }}>
-              <div style={{ backgroundColor: '#543310', borderRadius: '8px', padding: '8px', marginBottom: '8px', display: 'inline-block' }}>
-                <FileTextOutlined style={{ color: '#FFFFFF', fontSize: '38px' }} />
-              </div>
-              <Statistic
-                title={<h4 style={{ fontSize: '16px', color: '#543310' }}>Total Transaction</h4>}
-                value={10} // Mengisi nilai dari data API
-                valueStyle={{ color: '#64748B', fontSize: '18px', fontWeight: 'bold' }}
-              />
-            </div>
-          </Card>
-        </Col>
-        <Col span={16}>
-          <Card
-            style={{ 
-              backgroundColor: '#FFFFFF', 
-              borderRadius: '8px', 
-              padding: '8px', 
-              minHeight: '130px',
-              border: '3px solid #E2E8F0'
-            }}
-          >
-            <div style={{ textAlign: 'left' }}>
-              <div style={{ backgroundColor: '#543310', borderRadius: '8px', padding: '8px', marginBottom: '8px', display: 'inline-block' }}>
-                <BarChartOutlined style={{ color: '#FFFFFF', fontSize: '38px' }} />
-              </div>
-              <Statistic
-                title={<h4 style={{ fontSize: '16px', color: '#543310' }}>Total Monthly Income</h4>}
-                value={10} // Mengisi nilai dari data API
-                valueStyle={{ color: '#64748B', fontSize: '18px', fontWeight: 'bold' }}
-                formatter={value => `Rp. ${value.toLocaleString('id-ID')},00`}
-              />
-            </div>
-          </Card>
-        </Col>
-      </Row>
-
-      {/* Bagian bawah: 1 kotak */}
-      <Row gutter={[16, 16]} style={{ marginTop: '16px' }}>
-        <Col span={24}>
-          <Card
-            style={{ 
-              backgroundColor: '#FFFFFF', 
-              borderRadius: '8px', 
-              padding: '8px',
-              border: '3px solid #E2E8F0'
-            }}
-          >
-            <div style={{ textAlign: 'left' }}>
-              <div style={{ backgroundColor: '#543310', borderRadius: '8px', padding: '8px', marginBottom: '8px', display: 'inline-block' }}>
-                <DollarOutlined style={{ color: '#FFFFFF', fontSize: '38px' }} />
-              </div>
-              <Statistic
-                title={<h4 style={{ fontSize: '16px', color: '#543310' }}>All Total Income</h4>}
-                value={10} // Mengisi nilai dari data API
-                valueStyle={{ color: '#64748B', fontSize: '18px', fontWeight: 'bold' }}
-                formatter={value => `Rp. ${value.toLocaleString('id-ID')},00`}
-              />
-            </div>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-
-  );
+interface IconBoxProps {
+  icon: string;
+  title: string;
+  value: string | number;
 }
+
+const IconBox: React.FC<IconBoxProps> = ({ icon, title, value }) => {
+  return (
+    <div className="flex flex-col items-start p-4 border rounded-lg shadow-md bg-white">
+      <div className="w-10 h-10 bg-[#543310] flex items-center justify-center rounded mb-2">
+        <div className="w-"></div>
+        <img src={icon} alt={title} className="w-5 h-5" />
+      </div>
+      <h3 className="text-sm font-medium text-[#543310]">{title}</h3>
+      <p className="text-lg text-gray-500">{value}</p>
+    </div>
+  );
+};
+
+const Dashboard: React.FC = () => {
+  const data = [
+    {
+      icon: "/heroicons_clock-16-solid.svg", // Ganti dengan path ikon Anda
+      title: "Pending Transaction",
+      value: 50,
+    },
+    {
+      icon: "/unpaid.svg", // Ganti dengan path ikon Anda
+      title: "Unpaid Transaction",
+      value: 30,
+    },
+    {
+      icon: "/paid.svg", // Ganti dengan path ikon Anda
+      title: "Paid Transaction",
+      value: 70,
+    },
+    {
+      icon: "/income.svg", // Ganti dengan path ikon Anda
+      title: "Total Income",
+      value: "Rp. 1.000.000.000,00",
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {data.map((item, index) => (
+        <IconBox
+          key={index}
+          icon={item.icon}
+          title={item.title}
+          value={item.value}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Dashboard;
